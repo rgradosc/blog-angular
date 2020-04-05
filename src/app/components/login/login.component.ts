9e3820118a30160ca48b24ed4fd2e5fe27cb9082
rgradosc <gnr2092@gmail.com>
2020-04-05 18:50:35 -0500

    Create new components - Register component - Category component - Setting components in app.component - Initialize components construct with page title

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+
+  constructor() { 
+    this.page_title = 'Identificate';
+  }
 
   ngOnInit(): void {
   }
